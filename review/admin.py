7573c5d7e5519a8f9ce3157e37b3b4057feda1f7
hamjihyeon <gkawlgus01@naver.com>
2020-07-09 10:20:52 +0900

    startapp review

---
+++ review/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from review.models import Like, Where, Eat
+
+admin.site.register(Like)
+admin.site.register(Where)
+admin.site.register(Eat)
---
+++ review/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView, UpdateView, CreateView, ListView
+
+from review.models import Like, Eat, Where
+
+
+class LikeListView(ListView):
+    model = Like
+    paginate_by = 5
+
+
+class WhereCreateView(CreateView):
+    model = Where
+    fields = '__all__'
+    template_name = 'review/like_create.html'
+    success_url = reverse_lazy('review:list')
+
+class EatCreateView(CreateView):
+    model = Eat
+    fields = '__all__'
+    template_name = 'review/like_create.html'
+    success_url = reverse_lazy('review:list')
+
+
+class LikeUpdateView(UpdateView):
+    model = Like
+    fields = '__all__'
+    template_name_suffix = '_update'
+    success_url = reverse_lazy('review:list')
+
+
+class LikeDeleteView(DeleteView):
+    model = Like
+    success_url = reverse_lazy('review:list')
